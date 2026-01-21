0d1126af28c6980bd722eccbdb5cf0ca613fbbed
CatusElPoderoso <cactuselpoeroso@gmail.com>
2026-01-20 18:30:02 -0600

    df study

---
+++ kaggle/pandas/dataframe.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
+              'Sue': ['Pretty good.', 'Bland.']},
+             index=['Product A', 'Product B'])
+
+print(df)
